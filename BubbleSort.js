3e27408e94e846e09e0439b3da25b1b7a8f5e7a5
Mitanshu Kumar <mitanshukr@yahoo.com>
2021-08-12 20:34:40 +0530

    Added Bubble Sort

---
+++ BubbleSort.js
@@ -0,0 +1,59 @@
+const readline = require("./readline");
+readline(main);
+
+function swap(arr, p, q) {
+  const temp = arr[p];
+  arr[p] = arr[q];
+  arr[q] = temp;
+}
+
+function main(arr) {
+  let swapped;
+  let j = 0; //no. of sorted index or elements.
+  let swapCount = 0; //optional feature
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1 - j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        swapped = true;
+        swapCount++;
+      }
+    }
+    j++;
+  } while (swapped);
+
+  console.log(arr);
+  console.log("Total Number of Swaps required: " + swapCount);
+}
+
+/*****************************************************
+* Bubble Sorting:
+* ---------------------------------------
+* 1. Bubble Sorting Algorithm sorts by swapping adjacent elements, 
+     if they are out of order with respect to each other.
+*
+* 2. Bubble sort is not recommended to be used by experts.
+* 3. However, the Time and Space Complexity of Bubble Sort is same as
+     Insertion Sort, but experiments shows Bubble sorts performs very 
+     badly.(Wikipedia)
+*
+* 4. Time Complexity of Bubble Sort:
+        - Worst Case: O(n^2) -----> O(n^2) swaps.
+        - Average Case: O(n^2)
+        - Best Case: O(n) -----> No Swapping [O(1)] (Already Sorted).
+*
+* 5. Space Complexity: O(1) -----> 3 Variables.
+*
+* 6. Pseudo Code [Bubble Sort]:
+        do
+            swapped = false;
+            for i = 1 to indexOfUnsortedElement-1
+                if leftElement > rightElement
+                    swap(leftElement, rightElement);
+                    swapped = true;
+        while swapped;
+        END;
+*
+*
+******************************************************/
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A repo of Data Structure and Algorithm concepts with JavaScript.
 ### Sorting Algorithms
 1. Insertion Sort
 2. Merge Sort
+3. Bubble Sort
