c922f1a8bec25a7c310dcac71d14895ebb131dd3
Mitanshu Kumar <mitanshukr@yahoo.com>
2021-08-19 23:10:16 +0530

    Added Summation Recursion

---
+++ Recursion/2. Summation_of_numbers.js
@@ -0,0 +1,11 @@
+const readInput = require("./readInput");
+
+function summationFunc(n) {
+  if (n === 0) return 0;
+  return n + summationFunc(n - 1);
+}
+function main(input) {
+  console.log(`Summation of numbers 1 to ${input} is: {${summationFunc(input)}}`);
+}
+
+readInput(main);
